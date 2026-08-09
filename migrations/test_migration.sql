366b846299eed7c3189a5b9cc21a57ca5a96bb76
Your Name <kz6767824@gmail.com>
2026-08-09 15:21:38 +0500

    Live test of agent

--- migrations/test_migration.sql
+++ migrations/test_migration.sql
@@ -1,9 +1,1 @@
-ALTER TABLE orders
-  DROP COLUMN shipping_address,
-  ADD COLUMN referral_code VARCHAR(20),
-  RENAME COLUMN customer_notes TO internal_notes;
 
-ALTER TABLE customers
-  DROP COLUMN phone,
-  ALTER COLUMN region TYPE VARCHAR(100),
-  ADD COLUMN loyalty_tier VARCHAR(20) NOT NULL DEFAULT 'bronze';
